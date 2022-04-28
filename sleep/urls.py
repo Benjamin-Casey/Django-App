bd6c6e46733ea380ddaf2f599152aca0b59eda66
Benjamin Casey <benjamincasey55@gmail.com>
2022-04-28 13:57:42 +1000

    Added basic/inital views

--- sleep/urls.py
+++ sleep/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /sleep/
     path('', views.index, name = 'index'),
+
+    # /sleep/user/1/
+    path('user/<int:user_id>/', views.user_info, name='user_info'),
+
+    # /sleep/group/1/
+    path('group/<int:group_id>/', views.group_info, name='group_info'),
 ]
--- sleep/views.py
+++ sleep/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import User, Group
 
 def index(request):
-    return HttpResponse('Hello, BeddyByeBoys! You\'re at the sleep index!')
-    
+    users = User.objects.all()
+    # output = ', '.join([u for u in users])
+    return HttpResponse(users)
+
+# --- #
+
+def user_info(request, user_id):
+    return HttpResponse("This is user id %s" % user_id)
+
+def group_info(request, group_id):
+    return HttpResponse("This is group id %s" % group_id)
